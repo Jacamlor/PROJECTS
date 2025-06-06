7139265094efc46147ba90f39fbf4bd07df524ce
Jose Antonio Campos Lora <jacamlor@gmail.com>
2025-06-06 20:55:39 +0200

    Add files via upload

---
+++ Luppa/prueba.py
@@ -0,0 +1,17 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+# Manipulación y análisis de datos
+import pandas as pd
+import numpy as np
+
+# Valores nulos
+import missingno as msno
+
+# Visualización de datos
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+
+pd.read_csv(r'')
